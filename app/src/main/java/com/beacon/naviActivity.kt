da15d3f096837d2c2433ce6ad9ea018f1ac00d63
신근재 <60930743+Boknami@users.noreply.github.com>
2023-08-12 18:51:53 +0900

    [ADD] 네비게이션 바 추가

---
+++ app/src/main/java/com/beacon/naviActivity.kt
@@ -0,0 +1,80 @@
+package com.beacon
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.beacon.databinding.ActivityNaviBinding
+
+private const val TAG_MESSAGE = "message_fragment"
+private const val TAG_HOME = "main_fragment"
+private const val TAG_SETTINGS = "settings_fragment"
+
+class NaviActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityNaviBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityNaviBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        setFragment(TAG_HOME, mainFragment())
+
+        binding.navigationView.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.homeFragment -> setFragment(TAG_HOME, mainFragment())
+
+                R.id.messageFragment -> setFragment(TAG_MESSAGE, messageFragment())
+
+                R.id.settingsFragment-> setFragment(TAG_SETTINGS, settingsFragment())
+            }
+            true
+        }
+    }
+
+    private fun setFragment(tag: String, fragment: Fragment) {
+        val manager: FragmentManager = supportFragmentManager
+        val fragTransaction = manager.beginTransaction()
+
+        if (manager.findFragmentByTag(tag) == null){
+            fragTransaction.add(R.id.mainFrameLayout, fragment, tag)
+        }
+
+        val message = manager.findFragmentByTag(TAG_MESSAGE)
+        val home = manager.findFragmentByTag(TAG_HOME)
+        val settings = manager.findFragmentByTag(TAG_SETTINGS)
+
+        if (message != null){
+            fragTransaction.hide(message)
+        }
+
+        if (home != null){
+            fragTransaction.hide(home)
+        }
+
+        if (settings != null) {
+            fragTransaction.hide(settings)
+        }
+
+        if (tag == TAG_MESSAGE) {
+            if (message!=null){
+                fragTransaction.show(message)
+            }
+        }
+        else if (tag == TAG_HOME) {
+            if (home != null) {
+                fragTransaction.show(home)
+            }
+        }
+
+        else if (tag == TAG_SETTINGS){
+            if (settings != null){
+                fragTransaction.show(settings)
+            }
+        }
+
+        fragTransaction.commitAllowingStateLoss()
+    }
+}
