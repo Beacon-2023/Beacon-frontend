06d460201975ab3e4fba0d2813bcc21449808e6b
신근재 <shin75492@naver.com>
2023-08-15 00:20:08 +0900

    [ADD] 메시지 기록 관련(CardView)
    메시지 기록과 관련된 UI 생성.
    cardview + recyclerview

--- app/src/main/java/com/beacon/messageFragment.kt
+++ app/src/main/java/com/beacon/messageFragment.kt
@@ -5,19 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [messageFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class messageFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+
     private var param1: String? = null
     private var param2: String? = null
 
@@ -37,16 +35,21 @@ class messageFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_message, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Initialize RecyclerView
+        viewManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, true)
+        viewAdapter = cardViewAdapter()
+
+        recyclerView = view.findViewById<RecyclerView>(R.id.recyclerview_main).apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
+    }
+
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment messageFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             messageFragment().apply {
