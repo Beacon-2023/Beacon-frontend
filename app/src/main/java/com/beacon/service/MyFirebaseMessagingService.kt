4f52fefcd08ec2f3b90c8c79113b334db06b962b
신근재 <60930743+Boknami@users.noreply.github.com>
2023-08-21 02:08:33 +0900

    [Modify] FCM 기능 개선
    속도 개선(아직 미흡)및 보낸 메시지가 안 보이는 현상 해결.

--- app/src/main/java/com/beacon/service/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/beacon/service/MyFirebaseMessagingService.kt
@@ -11,30 +11,51 @@ import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
+    companion object {
+        private const val TAG = "테스트"
+        val notificationId = 123 // Use a value that makes sense for your app
+    }
+
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        Log.d("테스트", "Message received from: ${remoteMessage.from}")
+        Log.d(TAG, "==================================================")
+        Log.d(TAG, "FCM 수신 확인")
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        // Handle both data payload and notification payload
+        if (remoteMessage.notification != null) {
+            // Notification payload
+            val title = remoteMessage.notification?.title
+            val body = remoteMessage.notification?.body
+            showNotification(title, body)
+        } else if (remoteMessage.data.isNotEmpty()) {
+            // Data payload
+            val title = remoteMessage.data["title"]
+            val body = remoteMessage.data["body"]
+            showNotification(title, body)
+        }
+    }
+
+    private fun showNotification(title: String?, body: String?) {
+        Log.d(TAG, "Title: ${title}")
+        Log.d(TAG, "Body: ${body}")
 
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-        // Create a notification channel (for Android Oreo and above)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channelId = "channel_id"
-            val channel = NotificationChannel(channelId, "Channel Name", NotificationManager.IMPORTANCE_DEFAULT)
+            val channelName = "Channel Name"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(channelId, channelName, importance)
             notificationManager.createNotificationChannel(channel)
         }
 
-        // Define a unique notificationId
-        val notificationId = 123 // Use a value that makes sense for your app
-
-        // Build the notification
         val notificationBuilder = NotificationCompat.Builder(this, "channel_id")
-            .setContentTitle(remoteMessage.notification?.title)
-            .setContentText(remoteMessage.notification?.body)
+            .setContentTitle(title)
+            .setContentText(body)
             .setSmallIcon(R.drawable.icon_rain)
         // set other notification properties
 
-        // Show the notification
         notificationManager.notify(notificationId, notificationBuilder.build())
-        Log.d("테스트", "Notification shown with ID: $notificationId")
     }
 }
+
