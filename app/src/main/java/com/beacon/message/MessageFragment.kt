06d41c99e4186b742bcd7d4ec50e636a08ce2a7c
신근재 <shin75492@naver.com>
2023-09-02 22:14:44 +0900

    [ADD] api에서 받은 메시지 재가공 코드

--- app/src/main/java/com/beacon/message/MessageFragment.kt
+++ app/src/main/java/com/beacon/message/MessageFragment.kt
@@ -39,18 +39,49 @@ class messageFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        //여기서 재난 관련 API를 사용해서 재난 정보를 받아오고 items들을 구성한다!!
         val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerview_main)
-        val items = mutableListOf(
-            CardViewItem(R.drawable.icon_rain, "8월 13일 23시 10분", "'서울' 지역에 '폭우' 경보"),
-            CardViewItem(R.drawable.icon_rain, "8월 13일 23시 12분", "'서울' 지역에 '폭우' 경보"),
-            CardViewItem(R.drawable.icon_rain, "8월 13일 23시 13분", "'서울' 지역에 '폭우' 경보"),
-            CardViewItem(R.drawable.icon_rain, "8월 13일 23시 14분", "'서울' 지역에 '폭우' 경보"),
-            CardViewItem(R.drawable.icon_rain, "8월 13일 23시 15분", "'서울' 지역에 '폭우' 경보")
+        val items = mutableListOf<CardViewItem>()
+
+        val data = listOf(
+            Triple("호우", "August 13 23:10", "'Heavy rain' warning in 'Seoul' area"),
+            Triple("대설", "August 13 23:12", "'Heavy rain' warning in 'Seoul' area"),
+            Triple("지진", "August 13 23:13", "'Heavy rain' warning in 'Seoul' area"),
+            Triple("태풍", "August 13 23:14", "'Heavy rain' warning in 'Seoul' area"),
+            Triple("산불", "August 13, 23:15", "'Heavy rain' warning in 'Seoul' area")
         )
+
+        for (triple in data) {
+            val iconName = triple.first
+            val timestamp = triple.second
+            val message = triple.third
+
+            // Map the iconName to the appropriate resource ID
+            val iconResourceId = when (iconName) {
+                "호우" -> R.drawable.icon_rain
+                "대설" -> R.drawable.icon_heavysnow
+                "지진" -> R.drawable.icon_earthquake
+                "태풍" -> R.drawable.icon_typhoon
+                "산불" -> R.drawable.icon_buring
+                "민방위" -> R.drawable.icon_war
+                else -> R.drawable.icon_nodisaster
+            }
+
+            val cardViewItem = CardViewItem(iconResourceId, timestamp, message)
+            items.add(cardViewItem)
+        }
 
         val adapter = CardViewAdapter(items)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        //        val items = mutableListOf(
+//            CardViewItem(R.drawable.icon_rain, "8월 13일 23시 10분", "'서울' 지역에 '폭우' 경보"),
+//            CardViewItem(R.drawable.icon_rain, "8월 13일 23시 12분", "'서울' 지역에 '폭우' 경보"),
+//            CardViewItem(R.drawable.icon_rain, "8월 13일 23시 13분", "'서울' 지역에 '폭우' 경보"),
+//            CardViewItem(R.drawable.icon_rain, "8월 13일 23시 14분", "'서울' 지역에 '폭우' 경보"),
+//            CardViewItem(R.drawable.icon_rain, "8월 13일 23시 15분", "'서울' 지역에 '폭우' 경보")
+//        )
 
     }
 
