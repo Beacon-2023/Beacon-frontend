1288bd2704b7de8374c5e972b797621d68a6d110
신근재 <60930743+Boknami@users.noreply.github.com>
2023-09-05 17:26:49 +0900

    [ADD] 대피소 4,5등 추가 및 TODO제시

--- app/src/main/java/com/beacon/settings/guildLine/viewGuildLineActivity.kt
+++ app/src/main/java/com/beacon/settings/guildLine/viewGuildLineActivity.kt
@@ -18,10 +18,14 @@ class viewGuildLineActivity : AppCompatActivity() {
 
         val listView = findViewById<ListView>(com.beacon.R.id.listView_safezone)
 
+        //TODO : API로 가이드라인 받기
+        //TODO :받아온 JSON형태를 풀어서 나타내자
         val rankItemList: MutableList<RankItem> = ArrayList()
         rankItemList.add(RankItem(com.beacon.R.drawable.icon_1, "대피소1"))
         rankItemList.add(RankItem(com.beacon.R.drawable.icon_2, "대피소2"))
         rankItemList.add(RankItem(com.beacon.R.drawable.icon_3, "대피소3"))
+        rankItemList.add(RankItem(com.beacon.R.drawable.icon_4, "대피소4"))
+        rankItemList.add(RankItem(com.beacon.R.drawable.icon_5, "대피소5"))
 
         val adapter = RankListAdapter(this, rankItemList)
         listView.adapter = adapter
